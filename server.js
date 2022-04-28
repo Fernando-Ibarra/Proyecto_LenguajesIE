8ef58dd8ebdfbc81e255217313ca7a096dc6cacb
Damian <damianpeaf@gmail.com>
2022-04-27 18:56:23 -0600

    Server files

--- server.js
+++ server.js
@@ -1,35 +1,30 @@
-// Usando paquetes externos
-const path = require("path");
-const express = require("express");
-const WebSocket = require("ws");
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const WebSocket = require('ws');
 const app = express();
 
-// Declaracion de puertos
-const WS_PORT = 8888;
-const HTTP_PORT = 8000;
+const WS_PORT = process.env.WS_PORT;
+const HTTP_PORT = process.env.PORT;
 
-// Inicializando un objeto de web server
-const wsServer = new WebSocket.Server({port: WS_PORT}, ()=> console.log(`WS Server esta listo ${WS_PORT}`));
+const wsServer = new WebSocket.Server({ port: WS_PORT }, () => console.log(`WS Server en el puerto ${WS_PORT}`));
 
-// Declarando un arreglo de los dispositivos conectados
 let connectedClients = [];
-
-// Funcion para cuando se active el sevidor
-wsServer.on('connection', (ws, req)=>{
-    console.log('CONECTADO');
+wsServer.on('connection', (ws, req) => {
+    console.log('Connected');
     connectedClients.push(ws);
 
     ws.on('message', data => {
-        connectedClients.forEach((ws,i)=>{
-            if(ws.readyState === ws.OPEN){
+        connectedClients.forEach((ws, i) => {
+            if (ws.readyState === ws.OPEN) {
                 ws.send(data);
-            }else{
-                connectedClients.splice(i ,1);
+            } else {
+                connectedClients.splice(i, 1);
             }
         })
     });
 });
 
+app.get('/client', (req, res) => res.sendFile(path.resolve(__dirname, './index.html')));
+app.listen(HTTP_PORT, () => console.log(`HTTP server en el puerto ${HTTP_PORT}`));
 
-app.get('/client',(req,res)=>res.sendFile(path.resolve(__dirname, './client.html')));
-app.listen(HTTP_PORT, ()=> console.log(`HTTP ESTA LISTO at ${HTTP_PORT}`));
